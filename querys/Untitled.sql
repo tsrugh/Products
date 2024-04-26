5794b07ec05f28abe3cb2e3e9f39d0d65f15f49c
tsrugh <lucas_3600@hotmail.com.br>
2024-04-26 02:26:12 -0300

    [update] first structure

---
+++ querys/Untitled.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE product_sys;
+
+use product_sys;
+
+
+CREATE TABLE product(
+	productId int primary key auto_increment,
+    productName varchar (30) not null,
+    productPrice float,
+	userEmployeeId int,
+    foreign key (userEmployeeId) references userEmployee (userEmployeeId)
+);
+
+CREATE TABLE employee(
+	employeeId int primary key auto_increment,
+    employeeName varchar(50) not null,
+    employeeEmail varchar(50) not null,
+    employeePhone varchar(11)
+);
+
+CREATE TABLE userEmployee(
+	userEmployeeId int auto_increment,
+    userEmployeeLogin varchar(10),
+    userEmployeePass varchar(32),
+    userEmployeeRegistrationDate date DEFAULT(CURRENT_DATE),
+    userEmployeeIdEmployeeId int,
+    foreign key (userEmployeeIdEmployeeId) references employee (employeeId),
+    primary key (userEmployeeId, userEmployeeLogin)
+);
+
+drop table userEmployee;
+drop table product;
+
+INSERT INTO employee (employeeName, employeeEmail, employeePhone) values ('lucas pereira dos santos', 'lucaspdsts@gmail.com', '11971008836');
+INSERT INTO userEmployee (userEmployeeLogin, userEmployeePass, userEmployeeIdEmployeeId) values ('tsrugh', 'umaSenha123', 1);
+
+SELECT * FROM employee;
+SELECT * FROM userEmployee;
+
+SELECT employee.employeeName as userName from employee
+join userEmployee on userEmployee.userEmployeeId = employee.employeeId
+Where userEmployeeLogin = 'tsrugh';
+
+
+
+
